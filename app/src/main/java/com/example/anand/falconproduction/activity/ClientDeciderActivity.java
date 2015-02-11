bf2ee82a56e751ccd8dcd45a024dd78c4596be90
Anand mohan sinha <anand.s@tbitsglobal.com>
2015-02-11 11:14:28 +0530

    fix

--- app/src/main/java/com/example/anand/falconproduction/activity/ClientDeciderActivity.java
+++ app/src/main/java/com/example/anand/falconproduction/activity/ClientDeciderActivity.java
@@ -29,7 +29,7 @@ public class ClientDeciderActivity extends Activity {
       @Override
       public void onClick(View v) {
         String text = tokenText.getText().toString();
-        if (text.matches("anand||6081test||3081test||JSPLtest||JPLtest")) {
+        if (text.matches("anand||6081test||3081test||JSPLtest||JPLtest||UATtest")) {
           SharedPreferences sharedPreferences = getSharedPreferences(ApplicationConstants.appSharedPreference, MODE_PRIVATE);
           SharedPreferences.Editor tokenEditor = sharedPreferences.edit();
           tokenEditor.putString(ApplicationConstants.clientToken, text);
---
+++ app/src/main/java/com/example/anand/falconproduction/activity/SavedSearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.anand.falconproduction.activity;
+
+/**
+ * Created by anand on 10/2/15.
+ *
+ * List view to show saved searches of the user.
+ */
+public class SavedSearchActivity extends BaseDrawerActivity {
+
+
+
+  @Override
+  public long getBaId() {
+    return 0;
+  }
+}
